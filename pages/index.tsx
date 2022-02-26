d1e19e8bccd76f6f4f298f246e5d4e43dafe1d6f
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2022-02-26 08:50:41 -0800

    initial set up

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Test</h1>
+      
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+      
+      <div>{/* Player */}</div>
+    </div>
+  )
+}
+
+export default Home
